fb413db2d49ffc17f33690047e9a8ae931b5f686
ivan dyogtev <diogteff.ivan@yandex.ru>
2024-02-29 10:56:15 +0200

    changed main.class

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,15 +1,32 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.http.HttpStatus;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        // Создаем приложение
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
         });
-        // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
-        app.start(7070); // Стартуем веб-сервер
+
+//        app.get("/", ctx -> {
+//            ctx.status(403);
+//            ctx.header("KEY", "value");
+//        });
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParamAsClass("name", String.class).getOrDefault("Hello, World");
+            ctx.result("Hello " + name + "!");
+        });
+//        app.get("/admin", ctx -> {
+//            // Указываем статус вторым параметром
+//            ctx.redirect("/", HttpStatus.forStatus(302));
+//        });
+//        app.get("/", ctx -> {
+//            ctx.result("Hello World");
+//        });
+//        app.get("/users", ctx -> {
+//            var page = ctx.queryParamAsClass("page", Integer.class).getOrDefault(1);
+//        });
+        app.start(7070);
     }
 }
---
+++ src/main/java/org/example/hexlet/Lesson4/App.java
@@ -0,0 +1,7 @@
+package org.example.hexlet.Lesson4;
+
+import io.javalin.Javalin;
+
+public class App {
+
+}
