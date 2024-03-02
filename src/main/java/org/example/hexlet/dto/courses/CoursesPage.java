b5acaa614bac021ddc8b3d7def16acc69d0d02fd
ivan dyogtev <diogteff.ivan@yandex.ru>
2024-03-02 10:16:04 +0200

    fix bugs

--- src/main/java/org/example/hexlet/dto/courses/CoursesPage.java
+++ src/main/java/org/example/hexlet/dto/courses/CoursesPage.java
@@ -3,12 +3,10 @@ package org.example.hexlet.dto.courses;
 import lombok.NoArgsConstructor;
 import org.example.hexlet.model.Course;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 import java.util.List;
 
-@Getter
+
 public class CoursesPage {
     private List<Course> courses;
     private String header;
@@ -16,5 +14,13 @@ public class CoursesPage {
     public CoursesPage(List<Course> courses, String header) {
         this.courses = courses;
         this.header = header;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public String getHeader() {
+        return header;
     }
 }
