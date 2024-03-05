8ea6c603939034b5f639c03c2ccd411cc7265908
ivan dyogtev <diogteff.ivan@yandex.ru>
2024-03-05 16:08:55 +0200

    small fix

--- jte-classes/gg/jte/generated/ondemand/users/JteindexGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/users/JteindexGenerated.java
@@ -4,7 +4,7 @@ import org.example.hexlet.model.User;
 import org.example.hexlet.utils.NamedRoutes;
 public final class JteindexGenerated {
 	public static final String JTE_NAME = "users/index.jte";
-	public static final int[] JTE_LINE_INFO = {0,0,1,2,4,4,4,7,7,11,11,13,13,13,15,15,15,16,16,16,16,16,16,16,16,18,18,18,18,18,18,18,18,22,22,24,24,24,26,26,26,26,26,26,26,26,26,26,26,26,26,30,30,33,33,33,33,39,39,39};
+	public static final int[] JTE_LINE_INFO = {0,0,1,2,4,4,4,7,7,11,11,13,13,13,15,15,15,16,16,16,16,16,16,16,16,18,18,18,18,18,18,18,18,22,22,24,24,25,25,27,27,27,27,27,27,27,27,27,27,27,27,27,27,28,28,28,30,30,31,31,34,34,34,34,40,40,40};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, UsersPage page) {
 		jteOutput.writeContent("\r\n\r\n");
 		gg.jte.generated.ondemand.layout.JtepageGenerated.render(jteOutput, jteHtmlInterceptor, new gg.jte.html.HtmlContent() {
@@ -32,24 +32,31 @@ public final class JteindexGenerated {
 					jteOutput.writeContent("\"");
 				}
 				jteOutput.writeContent(">\r\n        </label>\r\n        <input type=\"submit\" value=\"Искать\">\r\n    </form>\r\n    ");
-				for (var user : page.getUserList()) {
-					jteOutput.writeContent("\r\n        <tr>\r\n            <td>");
-					jteOutput.setContext("td", null);
-					jteOutput.writeUserContent(user.getId());
-					jteOutput.writeContent("</td>\r\n            <td>\r\n                <a href=\"");
-					jteOutput.setContext("a", "href");
-					jteOutput.writeUserContent(NamedRoutes.usersPath());
-					jteOutput.setContext("a", null);
-					jteOutput.setContext("a", "href");
-					jteOutput.writeUserContent(user.getId());
-					jteOutput.setContext("a", null);
-					jteOutput.writeContent("\">");
-					jteOutput.setContext("a", null);
-					jteOutput.writeUserContent(user.getName());
-					jteOutput.writeContent(" ");
-					jteOutput.setContext("a", null);
-					jteOutput.writeUserContent(user.getLastName());
-					jteOutput.writeContent("</a>\r\n                <br>\r\n            </td>\r\n        </tr>\r\n    ");
+				if (page.getUserList().isEmpty()) {
+					jteOutput.writeContent("\r\n        <p>Пока не добавлено ни одного пользователя</p>\r\n    ");
+				} else {
+					jteOutput.writeContent("\r\n        ");
+					for (var user : page.getUserList()) {
+						jteOutput.writeContent("\r\n            <div>\r\n                <h2><a");
+						if (gg.jte.runtime.TemplateUtils.isAttributeRendered(NamedRoutes.userPath(user.getId()))) {
+							jteOutput.writeContent(" href=\"");
+							jteOutput.setContext("a", "href");
+							jteOutput.writeUserContent(NamedRoutes.userPath(user.getId()));
+							jteOutput.setContext("a", null);
+							jteOutput.writeContent("\"");
+						}
+						jteOutput.writeContent(">");
+						jteOutput.setContext("a", null);
+						jteOutput.writeUserContent(user.getName());
+						jteOutput.writeContent(" ");
+						jteOutput.setContext("a", null);
+						jteOutput.writeUserContent(user.getLastName());
+						jteOutput.writeContent("</a></h2>\r\n                <p>");
+						jteOutput.setContext("p", null);
+						jteOutput.writeUserContent(user.getEmail());
+						jteOutput.writeContent("</p>\r\n            </div>\r\n        ");
+					}
+					jteOutput.writeContent("\r\n    ");
 				}
 				jteOutput.writeContent("\r\n    </body>\r\n\r\n");
 			}
